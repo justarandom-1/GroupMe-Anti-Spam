60b6b1bba1c814466b20c8ec3e6e5d6e98787f23
just a random! <79994051+justarandom-1@users.noreply.github.com>
2024-10-14 13:26:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 TOKEN = "TOKEN HERE"
 GROUP_LIST_PATH = "groups.txt"
 BLACKLIST_PATH = "blacklist.txt"
+LAST_MESSAGES_PATH = "last_messages.txt"
 CONTACT_EMAIL = ""
 
 
@@ -9,14 +10,26 @@ import time
 import groupy
 from groupy.client import Client
 
-
 client = Client.from_token(TOKEN)
+self_id = client.user.get_me()['user_id']
 
 group_list = open(GROUP_LIST_PATH, "r").read().split('\n')
 ids = [group.id for group in client.groups.list()]
-monitored_groups = [client.groups.get(id) for id in group_list if id in ids]
-last_messages = [group.messages.list()[0] for group in monitored_groups]
 
+monitored_groups = [client.groups.get(id) for id in group_list if id in ids]
+
+last_messages = {}
+
+for group in monitored_groups:
+    last_messages[group.id] = group.messages.list()[0].id
+
+try:
+    last_messages_archive = open(LAST_MESSAGES_PATH, "r").read().split('\n')
+    for line in last_messages_archive:
+        data = line.split(' | ')
+        last_messages[data[0]] = data[1]
+except:
+    pass
 
 blacklist = open(BLACKLIST_PATH, "r").read().split('\n')
 for i in range(len(blacklist)):
@@ -25,57 +38,57 @@ for i in range(len(blacklist)):
     else:
         blacklist[i] = [blacklist[i]]
         
+def check(group, message):
+    text = message.text.upper()
+    if any(all([word in text for word in trigger]) for trigger in blacklist)\
+    and message.user_id not in [group.creator_user_id, self_id]:
+        try:
+            target = [member for member in group.members if member.user_id == message.user_id][0]
+            target_name = target.name
+            target.remove()
+            group.post(f"{target_name} removed for possible bot spam.")
+            if CONTACT_EMAIL:
+                group.post(f"(This was an automated action, email {CONTACT_EMAIL} if this was a mistake.)")
+        except:
+            pass
+
 print("On")
 
 while True:
-    time.sleep(1)
-    for i in range(len(monitored_groups)):
-        monitored_groups[i].refresh_from_server()
-        new_messages = list(monitored_groups[i].messages.list(since_id = last_messages[i].id))
-        if new_messages:        
-            last_messages[i] = new_messages[0]
+    time.sleep(5)
+    for group in monitored_groups:
+        group.refresh_from_server()
+        new_messages = list(group.messages.list(since_id = last_messages[group.id]))
+        if new_messages:
+            last_messages[group.id] = new_messages[0].id
             for message in new_messages:
 
                 if message.text.startswith('-setup'):
                     try:
                         group_data = message.text[message.text.index('https://groupme.com/join_group/') + 31:].split('/')
                         client.groups.join(group_data[0], group_data[1])
-                        monitored_groups[i].post(f"Joined group \'{client.groups.get(group_data[0]).name}\'")
+                        group.post(f"Joined group \'{client.groups.get(group_data[0]).name}\'")
 
                         open(GROUP_LIST_PATH, "a").write('\n' + str(group_data[0]))
                         monitored_groups.append(client.groups.get(group_data[0]))
                         last_messages.append(client.groups.get(group_data[0]).messages.list()[0] )
                     except:
-                        monitored_groups[i].post('Could not join group.')
+                        group.post('Could not join group.')
                 
                 if message.text.startswith('-ping'):
-                    monitored_groups[i].post('Bot is online.')
+                    group.post('Bot is online.')
                     
                 if message.text.startswith('-purge'):
-                    for m in monitored_groups[i].messages.list():
-                        text = m.text.upper()
-                        if any(all([y in text for y in x]) for x in blacklist)\
-                        and m.user_id not in [monitored_groups[i].creator_user_id, client.user.get_me()['user_id']]:
-                            try:
-                                target = [member for member in monitored_groups[i].members if member.user_id == m.user_id][0]
-                                target_name = target.name
-                                target.remove()
-                                if CONTACT_EMAIL:
-	                                monitored_groups[i].post(f"(This was an automated action, email {CONTACT_EMAIL} if this was a mistake.)")
-                            except:
-                                pass
-                        
-                    
-                text = message.text.upper()
-                if any(all([y in text for y in x]) for x in blacklist)\
-                and message.user_id not in [monitored_groups[i].creator_user_id, client.user.get_me()['user_id']]:                 
                     try:
-                        target = [member for member in monitored_groups[i].members if member.user_id == message.user_id][0]
-                        target_name = target.name
-                        target.remove()
-                        monitored_groups[i].post(f"{target_name} removed for possible bot spam.")
-                        if CONTACT_EMAIL:
-                            monitored_groups[i].post(f"(This was an automated action, email {CONTACT_EMAIL} if this was a mistake.)")
+                        target_group = group
+                        if len(message.text) > 7:
+                            target_group = client.groups.get(message.text[7:])
+
+                        for m in target_group.messages.list():
+                            check(group, m)                        
                     except:
-                        pass
-            break
+                        group.post('Unable to purge.')
+
+                check(group, message)
+
+    open(LAST_MESSAGES_PATH, "w").write('\n'.join([f"{id} | {last_messages[id]}" for id in last_messages.keys()]))
